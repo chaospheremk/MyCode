6b953687f70b68b65353d6c139bc7a2e9ab4506d
Doug Johnson <75006594+chaospheremk@users.noreply.github.com>
2022-06-28 11:58:39 -0500

    Add files via upload

---
+++ YubiKeyPIV-Reset.ps1
@@ -0,0 +1,62 @@
+# This script is for resetting a YubiKey for a PIV user who has locked their PIN or forgot their PIN. CMDB is
+# not updated in this script since the YubiKey remains assigned to the same user.
+#
+# 1. Enables all applications on the YubiKey.
+# 2. Resets all applications on the YubiKey.
+# 3. Disables all applications except for PIV.
+# 4. Directs the user to set their own PIV PIN.
+#
+# This script requires YubiKey Manager to be installed on the computer where the script runs. The default
+# location is C:\Program Files\Yubico\YubiKey Manager.
+#
+#################################################################################################################
+
+# Change to YubiKey Manager directory
+Set-Location "C:\Program Files\Yubico\YubiKey Manager"
+
+# Enables all applications over USB and NFC interfaces.
+.\ykman config usb -a -f
+.\ykman config nfc -a -f
+
+# Reset all applications as a good measure. User must remove and reinsert the YubiKey when prompted.
+Read-Host -Prompt "Remove and reinsert YubiKey, then press any key within 5 seconds of reinserting the Yubikey"
+.\ykman fido reset -f
+Write-Host "FIDO2 application was reset." -ForegroundColor Green
+.\ykman oath reset -f
+Write-Host "OATH application was reset." -ForegroundColor Green
+.\ykman openpgp reset -f
+Write-Host "OPENPGP application was reset." -ForegroundColor Green
+.\ykman otp delete 1 -f
+Write-Host "OTP slot 1 was reset." -ForegroundColor Green
+.\ykman otp delete 2 -f
+Write-Host "OTP slot 2 was reset." -ForegroundColor Green
+.\ykman piv reset -f
+Write-Host "PIV application was reset." -ForegroundColor Green
+
+# Disable all applications except for PIV over USB and NFC interfaces.
+Write-Host "Disabling all applications except for PIV..." -ForegroundColor Yellow
+.\ykman config usb -d FIDO2 -f
+.\ykman config usb -d OTP -f
+.\ykman config usb -d U2F -f
+.\ykman config usb -d OATH -f
+.\ykman config usb -d OPENPGP -f
+.\ykman config usb -d HSMAUTH -f
+Start-Sleep -Seconds 2
+.\ykman config nfc -d FIDO2 -f
+.\ykman config nfc -d OTP -f
+.\ykman config nfc -d U2F -f
+.\ykman config nfc -d OATH -f
+.\ykman config nfc -d OPENPGP -f
+.\ykman config nfc -d HSMAUTH -f
+Write-Host "All applications except for PIV have been disabled." -ForegroundColor Green
+
+# Generates CHUID for PIV
+.\ykman piv objects generate chuid -m 010203040506070801020304050607080102030405060708
+
+Write-Host "PIV YubiKey was successfully provisioned." -ForegroundColor Green
+
+# Enters the default PIV PIN and prompts the user to set their own PIN.
+Write-Host "Please set a new PIV PIN for the YubiKey that contains 6-8 alphanumeric characters." -ForegroundColor Yellow
+.\ykman piv access change-pin -P 123456
+Read-Host -Prompt "Press any key to exit"
+Exit
