da5d75c4adcf1a9fe886fec6745428487b96ba5b
Doug Johnson <75006594+chaospheremk@users.noreply.github.com>
2022-07-30 13:53:35 -0500

    updates

--- Remediate_LSA_Protection.ps1
+++ Remediate_LSA_Protection.ps1
@@ -1,8 +1,8 @@
 #=============================================================================================================================
 #
-# Script Name:     Detect_LSA_Protection.ps1
-# Description:     Determine whether LSA protection is on or not
-# Notes:           
+# Script Name:     Remediate_LSA_Protection.ps1
+# Description:     Turn on LSA protection
+# Notes:           Creates dword property on registry key and sets value to 1
 #
 #=============================================================================================================================
 
@@ -10,16 +10,10 @@
 $Path = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
 
 try {
-    if ((Get-ItemProperty -Path $Path).RunAsPPL -ne 1) {
-        Write-Host "RunAsPPL dword property is not set to 1"
-        exit 1
-    }
-    else {
-        Write-Host "RunAsPPL dword property is already set to 1"
-        exit 0
-    }
+New-ItemProperty -Path $Path -Value 00000001 -PropertyType dword -Name "RunAsPPL"
+exit 0
 }
-catch {
+catch{
     $errMsg = $_.Exception.Message
     Write-Error $errMsg
     exit 1
