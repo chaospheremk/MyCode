08afecc9b9d6ebe38f4d4c1ed8553a5a8f7619bc
Doug Johnson <75006594+chaospheremk@users.noreply.github.com>
2022-08-06 16:11:55 -0500

    Update

--- Detect_IE_RestrictLegacyJscript.ps1
+++ Detect_IE_RestrictLegacyJscript.ps1
@@ -1,22 +1,37 @@
 #=============================================================================================================================
 #
-# Script Name:     Detect_Edge_SharedArrayBuffers_setting.ps1
-# Description:     Determine whether following Microsoft Edge registry value exists for
-#                  setting: Specifies whether SharedArrayBuffers can be used in a non cross-origin-isolated context
+# Script Name:     Detect_Detect_IE_RestrictLegacyJscript.ps1
+# Description:     Determine whether following MS Security Guide registry value exists for
+#                  setting: Restrict legacy JScript execution for Office
 # Notes:           Part of the Microsoft Edge v103 baseline
 #
 #=============================================================================================================================
 
 # Define Variables
-$Path = "HKLM:\SOFTWARE\Policies\Microsoft\Edge"
+$Path = "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_LEGACY_JSCRIPT_PER_SECURITY_ZONE"
+$Processes = "excel.exe","mspub.exe","powerpnt.exe","onenote.exe","visio.exe","winproj.exe","winword.exe","outlook.exe","msaccess.exe"
 
 try {
-    if ((Get-ItemProperty -Path $Path).RunAsPPL -ne 0) {
-        Write-Host "SharedArrayBufferUnrestrictedAccessAllowed dword property is not set to 0"
+    if (!(Test-Path -Path $Path)) {
+        $RemediationNeeded = $true
+    }
+    
+    foreach($Process in $Processes) {
+        if ((!(Test-Path -Path "$Path\$Process")) -or ((Get-ItemPropertyValue -Path "$Path\$Process") -ne 69632)) {
+            $RemediationNeeded = $true
+            Write-Host "$Process registry property either not found or not set to 69632"
+        }
+        else {
+            Write-Host "$Process registry property found and set to 69632"
+        }
+    }
+
+    if ($RemediationNeeded) {
+        Write-Host "Either one of the required process keys doesn't exist or isn't set to 69632 - Remediation required"
         exit 1
     }
     else {
-        Write-Host "SharedArrayBufferUnrestrictedAccessAllowed dword property is already set to 0"
+        Write-Host "All required process keys exist and are set to 69632"
         exit 0
     }
 }
