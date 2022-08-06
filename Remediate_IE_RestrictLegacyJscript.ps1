2a763a9a12bcce4ce0c819f8a431583c3ec5dedf
Doug Johnson <75006594+chaospheremk@users.noreply.github.com>
2022-08-06 16:39:16 -0500

    Detection and remediation script s for M365 Apps for Enterprise baseline Legacy JScript block - Computer settings

---
+++ Remediate_IE_RestrictLegacyJscript.ps1
@@ -0,0 +1,70 @@
+#=============================================================================================================================
+#
+# Script Name:     Remediate_Edge_SharedArrayBuffers_setting.ps1
+# Description:     Disable Microsoft Edge
+#                  setting: Specifies whether SharedArrayBuffers can be used in a non cross-origin-isolated context
+# Notes:           Creates dword property on registry key and sets value to 1
+#
+#=============================================================================================================================
+
+# Define Variables
+$Path = "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_LEGACY_JSCRIPT_PER_SECURITY_ZONE"
+$Processes = "excel.exe", "mspub.exe", "powerpnt.exe", "onenote.exe", "visio.exe", "winproj.exe", "winword.exe", "outlook.exe", "msaccess.exe"
+
+try {
+    if (!(Test-Path -Path $Path)) {
+        New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl" -Name "FEATURE_RESTRICT_LEGACY_JSCRIPT_PER_SECURITY_ZONE"
+        Write-Host "Registry key: FEATURE_RESTRICT_LEGACY_JSCRIPT_PER_SECURITY_ZONE was successfully created"
+    }
+    else {
+        Write-Host "Registry key: FEATURE_RESTRICT_LEGACY_JSCRIPT_PER_SECURITY_ZONE already exists"
+    }
+
+    foreach ($Process in $Processes) {
+        if (!(Get-ItemProperty -Path $Path -Name $Process)) {
+            New-ItemProperty -Path $Path -PropertyType dword -Name $Process -Value 69632
+            Write-Host "$Process registry property successfully created and value set to 69632"
+            exit 0
+        }
+        elseif ((Get-ItemPropertyValue -Path $Path -Name $Process) -ne 69632) {
+            Set-ItemProperty -Path $Path -Name $Process -Value 69632 -Force
+            Write-Host "$Process registry property found and set to 69632"
+            exit 0
+        }
+        else {
+            Write-Host "$Process registry property found and was already set to 69632"
+            exit 0
+        }
+    }
+}
+catch {
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+try {
+    New-ItemProperty -Path $Path -Value 00000000 -PropertyType dword -Name "SharedArrayBufferUnrestrictedAccessAllowed"
+    Write-Host "SharedArrayBufferUnrestrictedAccessAllowed dword property was successfully set to 0"
+    exit 0
+}
+catch{
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
