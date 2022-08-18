b40a6d75499b5e34f61b7d539aa84dda6787a66d
Doug Johnson <75006594+chaospheremk@users.noreply.github.com>
2022-08-18 17:54:23 -0500

    update

--- GraphAutomationAuthenticate.ps1
+++ GraphAutomationAuthenticate.ps1
@@ -1,5 +1,5 @@
-$tenantID = "092ec6d1-91bb-41cc-a354-90068582d5c8"
-$applicationID = "9831de77-e353-4cda-b5f9-16580cdbb8ea"
-$thumbprint = "B747A10321E5220D091244321B4D9D8D5C99ADE9"
+$TenantId = "092ec6d1-91bb-41cc-a354-90068582d5c8"
+$ApplicationId = "9831de77-e353-4cda-b5f9-16580cdbb8ea"
+$CertificateThumbprint = "B747A10321E5220D091244321B4D9D8D5C99ADE9"
  
-Connect-MgGraph -ClientID $applicationID -TenantId $tenantID -CertificateThumbprint $thumbprint
+Connect-MgGraph -ClientID $ApplicationId -TenantId $TenantId -CertificateThumbprint $CertificateThumbprint
