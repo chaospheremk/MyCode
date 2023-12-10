9af4b2850a22a2b79f3e98587d19c6d414d46204
Doug Johnson <75006594+chaospheremk@users.noreply.github.com>
2023-12-10 15:10:20 -0600

    CMTrace remediation

---
+++ ProactiveRemediations/Install-CMTrace/Remediate_CMTrace.ps1
@@ -0,0 +1,19 @@
+<#
+    Version: 1.0
+    Author: 
+    - Joey Verlinden (joeyverlinden.com)
+    - Andrew Taylor (andrewstaylor.com)
+    - Florian Slazmann (scloud.work)
+    - Jannik Reinhard (jannikreinhard.com)
+    Script: install-cmtrace-remediate.ps1
+    Description: Installs CMTrace to c:\windows\system32
+    Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+    Version 1.0: Init
+    Run as: System
+    Context: 64 Bit
+#> 
+
+## Download CMTrace from the following repository and copy it in your own repo "https://github.com/andrew-s-taylor/public/raw/main/Troubleshooting/CMTrace.exe"
+$ownRepoUri = "https://github.com/chaospheremk/MyCode/blob/main/CMTrace/cmtrace.exe"
+
+invoke-webrequest -uri $ownRepoUri -outfile "C:\Windows\System32\cmtrace.exe"
