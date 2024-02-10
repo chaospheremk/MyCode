851aa1b4b4b057e2ed9d567d17b9ae96549857d6
Doug Johnson <75006594+chaospheremk@users.noreply.github.com>
2024-02-10 15:46:53 -0600

    update

--- Win32Apps/GraphAutomationCertInstall/PfxRemove.ps1
+++ Win32Apps/GraphAutomationCertInstall/PfxRemove.ps1
@@ -1,2 +1,4 @@
 Get-ChildItem Cert:\CurrentUser\My | Where-Object { $_.Subject -match 'GraphAutomation' } | Remove-Item
 Remove-Item -Path HKCU:\Software\dougjohnsonme\GraphAutomationCert
+
+Invoke-WUJob -ComputerName util.dougjohnson.me -Credential $cred -Script { ipmo PSWindowsUpdate; Install-WindowsUpdate -AcceptAll -Install -AutoReboot | Out-File "C:\Windows\PSWindowsUpdate.log"} -RunNow -Confirm:$false -Verbose
